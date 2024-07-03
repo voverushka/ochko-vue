da1f066c83824b2a8fad9361dff74aa5fead3915
Gitana Kovanaite <gkovanaite@Gitanas-MacBook-Air.local>
2024-07-03 16:16:04 +0100

    test okta

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import TheGame from './components/Game/TheGame.vue'
-</script>
 
+const addItem = () => {
+  fetch('https://dev-54811785.okta.com', {
+    method: 'GET'
+  })
+    .then(() => {
+      console.log('Logiged in')
+    })
+    .catch((e) => {
+      console.log('Error ', e)
+    })
+}
+</script>
 <template>
   <main>
+    <button type="submit" v-on:click="addItem">Test</button>
     <the-game />
   </main>
 </template>
